d2c213977bea96705d766b17a7598700c672a1ad
PommesPeter <PommesPeter@users.noreply.huggingface.co>
2024-05-18 09:57:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -569,7 +569,7 @@ def main():
                 examples_per_page=22,
             )
 
-        @spaces.GPU(duration=120)
+        @spaces.GPU(duration=180)
         def on_submit(*infer_args, progress=gr.Progress(track_tqdm=True),):
             result = infer_ode(args, infer_args, text_encoder, tokenizer, vae, model)
             if isinstance(result, ModelFailure):
